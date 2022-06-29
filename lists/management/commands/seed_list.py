826387ec49b3d39bc63c28297831f2b4fe6af4ea
goodbyelenin89 <91268326+goodbyelenin89@users.noreply.github.com>
2022-06-29 19:22:28 +0300

    11

--- lists/management/commands/seed_list.py
+++ lists/management/commands/seed_list.py
@@ -1,7 +1,5 @@
 import random
 from django.core.management.base import BaseCommand
-from django.contrib.admin.utils import flatten
-from django_seed import Seed
 from lists import models as list_models
 from users import models as user_models
 from rooms import models as room_models
